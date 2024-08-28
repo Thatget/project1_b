1be02b5d7f3526a158e903461e9924ff89cd6f6f
Philip <quyet.nguyen.7712826@gmail.com>
2024-08-28 17:14:30 +0700

    chat socket

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+// @ts-ignore
+import { Server as p2pServer } from 'socket.io-p2p-server'
 
 dotenv.config();
 
@@ -10,20 +12,25 @@ const port = process.env.PORT || 3002;
 
 const server = createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  }
+});
+
+io.use(p2pServer);
 
 io.on('connection', (socket) => {
   console.log('a new client connected');
-
   socket.on('message', (arg) => {
-    console.log(arg);
+    console.log(arg)
+    io.emit('answer', { data: 'FFFFFFFFF' });
   })
 
   socket.on('disconnect', () => {
     console.log("disconect");
   })
 })
-
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
